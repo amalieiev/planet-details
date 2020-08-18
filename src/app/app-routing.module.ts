a08c1c70d07c4ba178a622facc282e960e326218
Artem Malieiev <artem.malieiev@namecheap.com>
2020-08-18 17:15:59 +0300

    Configured App module. Created routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanetsComponent } from './planets/planets.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PlanetsComponent, canActivate: [AuthGuard]},
+  {path: 'auth', component: AuthComponent},
+  // Using details as separate module to improve app first load time
+  {path: 'details', loadChildren: () => import('./details/details.module').then(m => m.DetailsModule), canActivate: [AuthGuard]},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthComponent } from './auth/auth.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PlanetsService } from './planets/planets.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { PlanetCardComponent } from './planets/planet-card/planet-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    PlanetCardComponent,
+    NotFoundComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
-  providers: [],
+  providers: [
+    PlanetsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
