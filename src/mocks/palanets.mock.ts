138a0159a7085a76962a083befadaf22549f9527
Artem Malieiev <artem.malieiev@namecheap.com>
2020-08-18 17:00:38 +0300

    Created Planets mock for unit tests

---
+++ src/mocks/palanets.mock.ts
@@ -0,0 +1,13 @@
+import { Planet } from '../app/common/models/planet';
+
+export const planetsMock: Planet[] = [
+  {
+    name: 'name',
+    image: 'image',
+    description: 'description',
+    characters: 'characters',
+    system: 'system',
+    region: 'region',
+    species: 'species'
+  }
+];
