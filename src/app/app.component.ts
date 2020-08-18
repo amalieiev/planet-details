2d3fec7b929c2360139b70ccbf136a73d2ecbc0e
Artem Malieiev <artem.malieiev@namecheap.com>
2020-08-18 17:15:21 +0300

    Updated App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'planet-details';
+export class AppComponent implements OnInit {
+  public email$: Observable<string>;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.email$ = this.authService.email$;
+  }
+
+  public handleSignOut(): void {
+    this.authService.signOut();
+  }
 }
