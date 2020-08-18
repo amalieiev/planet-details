91af7545c8549e9d053dd7760af770159568c04d
Artem Malieiev <artem.malieiev@namecheap.com>
2020-08-18 17:12:32 +0300

    Created PlanetCard component

---
+++ src/app/planets/planet-card/planet-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Planet } from '../../common/models/planet';
+
+@Component({
+  selector: 'app-planet-card',
+  templateUrl: './planet-card.component.html',
+  styleUrls: ['./planet-card.component.scss']
+})
+export class PlanetCardComponent implements OnInit {
+
+  @Input() planet: Planet;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
