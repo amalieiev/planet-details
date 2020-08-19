672267ef983ca5d37dab6ead61e979b42e20edb0
Artem Malieiev <artem.malieiev@namecheap.com>
2020-08-19 15:52:08 +0300

    Updated paths.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PlanetsComponent } from './planets/planets.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AuthGuard } from './auth/auth.guard';
-import { AuthComponent } from './auth/auth.component';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './components/auth/auth.guard';
+import { AuthComponent } from './components/auth/auth.component';
 
 const routes: Routes = [
   {path: '', component: PlanetsComponent, canActivate: [AuthGuard]},
   {path: 'auth', component: AuthComponent},
   // Using details as separate module to improve app first load time
-  {path: 'details', loadChildren: () => import('./details/details.module').then(m => m.DetailsModule), canActivate: [AuthGuard]},
+  {path: 'details', loadChildren: () => import('./modules/details/details.module').then(m => m.DetailsModule), canActivate: [AuthGuard]},
   {path: '**', component: NotFoundComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PlanetsComponent } from './planets/planets.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AuthComponent } from './auth/auth.component';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthComponent } from './components/auth/auth.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { PlanetsService } from './planets/planets.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { PlanetCardComponent } from './planets/planet-card/planet-card.component';
+import { PlanetsService } from './components/planets/planets.service';
+import { PlanetCardComponent } from './components/planets/planet-card/planet-card.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlanetsComponent,
     PlanetCardComponent,
+    AuthComponent,
     NotFoundComponent,
-    AuthComponent
+    CardComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
   ],
   providers: [
     PlanetsService
